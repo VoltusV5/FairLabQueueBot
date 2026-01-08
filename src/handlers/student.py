b1f81f4d13e779a601b2435f55fc9db8834c221f
Tereshchenko Maxim <maxt.spb@gmail.com>
2026-01-08 15:25:05 +0300

    Add initial bot implementation with configuration and command handlers

--- src/handlers/student.py
+++ src/handlers/student.py
@@ -3,3 +3,11 @@
 запись/выход из очереди, просмотр места и статистики, просмотр текущей очереди.
 Не уверен, что реально надо
 """
+
+from aiogram import Router
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message
+from src.lexicon import LEXICON_RU
+
+# Инициализируем роутер уровня модуля
+router = Router()
