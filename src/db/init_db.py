24e8c09c6bcb67a5efc3470dee0d1d3d600b3894
Tereshchenko Maxim <maxt.spb@gmail.com>
2026-01-09 12:55:36 +0300

    Добавил заполнение User Subject

--- src/db/init_db.py
+++ src/db/init_db.py
@@ -1,7 +1,7 @@
 """Создание/инициализация БД."""
 
 from sqlalchemy import Integer, String
-from sqlalchemy import ForeignKey, DateTime, JSON, Boolean
+from sqlalchemy import ForeignKey, DateTime, JSON, Boolean, UniqueConstraint
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from datetime import datetime
 
@@ -15,9 +15,11 @@ class Subject(Base):
 
     __tablename__ = "subject"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    chat_id: Mapped[int] = mapped_column(Integer)
-    name: Mapped[str] = mapped_column(String)
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, index=True, autoincrement=True)
+    chat_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    subject_name: Mapped[str] = mapped_column(
+        String, nullable=False, unique=True)
 
 
 class Queue(Base):
@@ -25,14 +27,23 @@ class Queue(Base):
 
     __tablename__ = "queue"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True)
 
     subject_id: Mapped[int] = mapped_column(ForeignKey("subject.id"))
+    chat_id: Mapped[int] = mapped_column(ForeignKey("subject.chat_id"))
 
-    chat_id: Mapped[int] = mapped_column(Integer)
-    data: Mapped[datetime] = mapped_column(DateTime)
+    lesson_date: Mapped[datetime] = mapped_column(DateTime)
     close_at: Mapped[datetime] = mapped_column(DateTime)
     status: Mapped[str] = mapped_column(String)
+
+    """Для того, чтобы исключить лишние проверки добавляем правило, что
+    Если subject_id, lesson_date, chat_id совпадают, то выдаём исключение
+    """
+    __table_args__ = (
+        UniqueConstraint("subject_id", "chat_id", "lesson_date",
+                         name="unique_subject_chat_lesson"),
+    )
 
 
 class User(Base):
@@ -47,7 +58,7 @@ class User(Base):
     real_name: Mapped[str] = mapped_column(String, unique=True, nullable=True)
     is_admin: Mapped[bool] = mapped_column(
         Boolean, nullable=False, default=False)
-    chat_id: Mapped[int] = mapped_column(Integer)
+    chat_id: Mapped[int] = mapped_column(Integer, nullable=False)
 
 
 class SubmissionAttempt(Base):
@@ -55,7 +66,8 @@ class SubmissionAttempt(Base):
 
     __tablename__ = "queue_history"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     subject_id: Mapped[int] = mapped_column(ForeignKey("subject.id"))
     history_position: Mapped[dict] = mapped_column(JSON)
@@ -66,7 +78,8 @@ class Pay(Base):
 
     __tablename__ = "pay"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     status: Mapped[bool] = mapped_column(Boolean)
     date_pay: Mapped[datetime] = mapped_column(DateTime)
